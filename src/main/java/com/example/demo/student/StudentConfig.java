b3dbc0b51939706ecb8f7243e488050ab06c99f9
fasowllaza <52316053+fasowllaza@users.noreply.github.com>
2021-08-03 15:31:33 +0700

    Create Feature Added

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -18,14 +18,12 @@ import java.util.List;
                 Student mariam = new Student(
                         "Mariam",
                         "mariam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        21
+                        LocalDate.of(2000, Month.JANUARY,5)
                 );
                 Student alex = new Student(
                         "Alex",
                         "alex@gmail.com",
-                        LocalDate.of(2011, Month.JANUARY,5),
-                        24
+                        LocalDate.of(2011, Month.JANUARY,5)
                 );
                 repository.saveAll(List.of(mariam, alex));
             };
